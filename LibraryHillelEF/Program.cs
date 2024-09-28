6952bbbacba8a0780700e3b9861c90410d08f0ae
Misha Vasilkov <mishytka.vasilkov@gmail.com>
2024-09-28 19:13:37 +0300

    Add project files.

---
+++ LibraryHillelEF/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryHillelEF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
