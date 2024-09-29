7410984fd464418a66205b1116e22b6b2dca1d72
Misha Vasilkov <mishytka.vasilkov@gmail.com>
2024-09-29 23:26:22 +0300

    add some realisation

--- LibraryHillelEF/LibrarianFuncs.cs
+++ LibraryHillelEF/LibrarianFuncs.cs
@@ -6,6 +6,7 @@ using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Reflection.Metadata.BlobBuilder;
 
 namespace LibraryHillelEF
 {
@@ -24,24 +25,31 @@ namespace LibraryHillelEF
             }
             
         }
-        //public void UpdateBook(Book book, )
-        //{
-        //    using (var unitOfWork = new UnitOfWork())
-        //    {
-        //        var bookrepos = new BookRepository(unitOfWork);
-        //        await bookrepos.Create(new Book
-        //        {
-        //            Title = title,
-        //            Authors = authors,
-        //            City = city,
-        //            Country = country,
-        //            PublisherCode = PublisherCode,
-        //            PublisherCodeType = publisherCodeType,
-        //            YearOfPublication = yearOfPublication
-        //        });
-        //        await unitOfWork.SaveAsync();
-        //    }
-        //}
+        public async Task UpdateBook(Book oldbook, Book newchanges)
+        {
+            using (var unitOfWork = new UnitOfWork())
+            {
+                if(newchanges.City != null)
+                    oldbook.City = newchanges.City;
+                if (newchanges.Country!= null)
+                    oldbook.Country = newchanges.Country; 
+                if (newchanges.TakenBook != null)
+                    oldbook.TakenBook = newchanges.TakenBook;
+                if (newchanges.PublisherCodeType != null)
+                    oldbook.PublisherCodeType = newchanges.PublisherCodeType;
+                if (newchanges.PublisherCode != null)
+                    oldbook.PublisherCode = newchanges.PublisherCode;
+                if (newchanges.Authors != null)
+                    oldbook.Authors = newchanges.Authors;
+                if (newchanges.Title != null)
+                    oldbook.Title = newchanges.Title;
+                if (newchanges.YearOfPublication != null)
+                    oldbook.YearOfPublication = newchanges.YearOfPublication;
+                var bookrepos = new BookRepository(unitOfWork);
+                await bookrepos.Update(oldbook);
+                await unitOfWork.SaveAsync();
+            }
+        }
         public async Task AddAuthor(string firstName, string lastName, string? middleName, DateOnly? birthday, List<Book>? books )
         {
             using (var unitOfWork = new UnitOfWork())
@@ -58,21 +66,78 @@ namespace LibraryHillelEF
                 await unitOfWork.SaveAsync();
             }
         }
-        public void UpdateAuthor()
+        public async Task UpdateAuthor(Author oldauthor, Author newchanges)
         {
-
+            using (var unitOfWork = new UnitOfWork())
+            {
+                if (newchanges.Birthday != null)
+                    oldauthor.Birthday = newchanges.Birthday;
+                if (newchanges.FirstName != null)
+                    oldauthor.FirstName = newchanges.FirstName;
+                if (newchanges.LastName != null)
+                    oldauthor.LastName = newchanges.LastName;
+                if (newchanges.MiddleName != null)
+                    oldauthor.MiddleName = newchanges.MiddleName;
+                if (newchanges.Books != null)
+                    oldauthor.Books = newchanges.Books;
+                
+                var authorrepos = new AuthorRepository(unitOfWork);
+                await authorrepos.Update(oldauthor);
+                await unitOfWork.SaveAsync();
+            }
         }
-        public void AddReader()
+        public async Task AddReader(List<TakenBook> takenBook, string firstName, string lastName, DocumentType typeOfDocument, string documentNumber, string login, string password, string email)
         {
-
+            using (var unitOfWork = new UnitOfWork())
+            {
+                var readerrepos = new ReaderRepository(unitOfWork);
+                await readerrepos.Create(new Reader
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    TakenBook = takenBook,
+                    TypeOfDocument = typeOfDocument,
+                    DocumentNumber = documentNumber,
+                    Email = email,
+                    Password = password,
+                    Login = login
+                });
+                await unitOfWork.SaveAsync();
+            }
         }
-        public void UpdateReader()
+        public async Task UpdateReader(Reader oldreader, Reader newchanges)
         {
+            using (var unitOfWork = new UnitOfWork())
+            {
+                if (newchanges.TakenBook != null)
+                    oldreader.TakenBook = newchanges.TakenBook;
+                if (newchanges.FirstName != null)
+                    oldreader.FirstName = newchanges.FirstName;
+                if (newchanges.LastName != null)
+                    oldreader.LastName = newchanges.LastName;
+                if (newchanges.TypeOfDocument != null)
+                    oldreader.TypeOfDocument = newchanges.TypeOfDocument;
+                if (newchanges.DocumentNumber != null)
+                    oldreader.DocumentNumber = newchanges.DocumentNumber;
+                if (newchanges.Email != null)
+                    oldreader.Email = newchanges.Email;
+                if (newchanges.Login != null)
+                    oldreader.Login = newchanges.Login;
+                if (newchanges.Password != null)
+                    oldreader.Password = newchanges.Password;
 
+                var readerrepos = new ReaderRepository(unitOfWork);
+                await readerrepos.Update(oldreader);
+                await unitOfWork.SaveAsync();
+            }
         }
-        public void DeleteReader()
+        public async Task DeleteReader(int id)
         {
-
+            using (var unitOfWork = new UnitOfWork())
+            {
+                var readerrepos = new ReaderRepository(unitOfWork);
+                await readerrepos.Delete(id);
+            }
         }
         public void GetHistoryTakenBook()
         {
